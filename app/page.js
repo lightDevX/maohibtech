64cde08c757017e503c45b92200406e90c6df7d3
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-01-13 13:35:02 +0600

    upgrade to typescript

--- app/page.js
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <>
-      <p className=" font-black">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, eius.</p>
-    </>
-  );
-}
