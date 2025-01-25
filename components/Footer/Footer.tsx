ccd593e49c23b0f2c773835a79545528d958c8bb
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-01-25 20:45:03 +0600

    footer check

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -8,7 +8,14 @@ const Footer = () => {
         <div className="logo w-full h-full">
           <Image src={logo} alt="logo" width={256} height={48} />
         </div>
-        <div className="services">service</div>
+        <div className="services">
+          <ul>
+            <li>Services</li>
+            <li>Service 1</li>
+            <li>Service 2</li>
+            <li>Service 3</li>
+          </ul>
+        </div>
         <div className="information">information</div>
         <div className="social">social</div>
       </div>
