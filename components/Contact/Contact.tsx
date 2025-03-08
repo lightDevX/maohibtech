f9ff9ca3b41e5c95ad61e13fa5f8fb29769fac3e
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-08 18:01:12 +0600

    code update

---
+++ components/Contact/Contact.tsx
@@ -0,0 +1,13 @@
+const Contact = () => {
+  return (
+    <>
+      <div classNameName="text-center mt-8">
+        <form>
+          <div className="space-y-12"></div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
