6f38c508113956cb50a247e7b0dc79e79574d456
lightdevx <khan.m.hasanuzzaman@gmail.com>
2025-03-16 09:34:38 +0600

    service page update

---
+++ app/(services)/clipping-path/page.tsx
@@ -0,0 +1,9 @@
+const ClippingPath = () => {
+  return (
+    <div className="container mx-auto">
+      <h1>ClippingPath</h1>
+    </div>
+  );
+};
+
+export default ClippingPath;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Contact from "@/components/Contact/contact";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container m-2 mx-auto flex items-center justify-center p-2 text-white">
       <Contact />
+      <Link href="/clipping-path">Clipping Path</Link>
     </div>
   );
 }
